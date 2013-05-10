566e082f189e431b685fac9e0ec9b1f3fb27ce29
Hardeep Singh <ootz0rz@gmail.com>
2013-05-10 15:25:19 -0400

    looks like it has to be .net 3.5 to work also added com reference

--- src/Program.cs
+++ src/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using iTunesLib;
 
 namespace iTunes_Tag_Manager
 {
@@ -9,6 +10,12 @@ namespace iTunes_Tag_Manager
     {
         static void Main(string[] args)
         {
+            IiTunes app = new iTunesAppClass();
+
+            IITTrack track = app.CurrentTrack;
+
+            Console.WriteLine(string.Format("Current Track: {0} - {1}", track.Name, track.Artist));
+
         }
     }
 }
