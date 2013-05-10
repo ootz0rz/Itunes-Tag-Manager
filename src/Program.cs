f9095b4f2bcb96e7d37e0fcaef8a1e209c4f526d
Hardeep Singh <ootz0rz@gmail.com>
2013-05-10 15:00:04 -0400

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace iTunes_Tag_Manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
