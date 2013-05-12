d709ddc0faa11e04fa89026623db4455224d610f
Hardeep Singh <ootz0rz@gmail.com>
2013-05-11 22:07:03 -0400

    more tests

--- tests/CommentParserTests.cs
+++ tests/CommentParserTests.cs
@@ -24,14 +24,106 @@ namespace tests
             string pre = "stuff before\n";
             string post = "\nstuff after";
 
-            string testComment = string.Format(
-                "{0}{1}{2}{3}{4}"
-                , pre
-                , CommentParser.CommentDataPreKeyString
-                , json
-                , CommentParser.CommentDataPostKeyString
-                , post
-            );
+            string testComment = string.Format("{0}{1}{2}{3}{4}", pre, CommentParser.CommentDataPreKeyString, json, CommentParser.CommentDataPostKeyString, post);
+
+            Console.WriteLine("TEST DATA:");
+            Console.WriteLine(string.Format("JSON:" + Environment.NewLine + "{0}", json));
+            Console.WriteLine(string.Format("PRE: {0}", pre));
+            Console.WriteLine(string.Format("POST: {0}", post));
+
+            CommentParser cp = new CommentParser(testComment);
+
+            Assert.AreEqual(cd.Rating, cp.CommentData.Rating);
+            Assert.AreEqual(pre, cp.PreData);
+            Assert.AreEqual(post, cp.PostData);
+            Assert.AreEqual(json, cp.JsonString);
+
+            Console.WriteLine(Environment.NewLine + Environment.NewLine + "ACTUAL DATA:");
+            Console.WriteLine(string.Format("JSON:" + Environment.NewLine + "{0}", cp.JsonString));
+            Console.WriteLine(string.Format("PRE: {0}", cp.PreData));
+            Console.WriteLine(string.Format("POST: {0}", cp.PostData));
+        }
+
+        [TestMethod]
+        public void Comment_With_Existing_Valid_Data_No_Pre()
+        {
+            CommentData cd = new CommentData()
+            {
+                Rating = 100
+            };
+
+            string json = JsonConvert.SerializeObject(cd);
+
+            string pre = "";
+            string post = "\nstuff after";
+
+            string testComment = string.Format("{0}{1}{2}{3}{4}", pre, CommentParser.CommentDataPreKeyString, json, CommentParser.CommentDataPostKeyString, post);
+
+            Console.WriteLine("TEST DATA:");
+            Console.WriteLine(string.Format("JSON:" + Environment.NewLine + "{0}", json));
+            Console.WriteLine(string.Format("PRE: {0}", pre));
+            Console.WriteLine(string.Format("POST: {0}", post));
+
+            CommentParser cp = new CommentParser(testComment);
+
+            Assert.AreEqual(cd.Rating, cp.CommentData.Rating);
+            Assert.AreEqual(pre, cp.PreData);
+            Assert.AreEqual(post, cp.PostData);
+            Assert.AreEqual(json, cp.JsonString);
+
+            Console.WriteLine(Environment.NewLine + Environment.NewLine + "ACTUAL DATA:");
+            Console.WriteLine(string.Format("JSON:" + Environment.NewLine + "{0}", cp.JsonString));
+            Console.WriteLine(string.Format("PRE: {0}", cp.PreData));
+            Console.WriteLine(string.Format("POST: {0}", cp.PostData));
+        }
+
+        [TestMethod]
+        public void Comment_With_Existing_Valid_Data_No_Post()
+        {
+            CommentData cd = new CommentData()
+            {
+                Rating = 100
+            };
+
+            string json = JsonConvert.SerializeObject(cd);
+
+            string pre = "stuff before\n";
+            string post = "";
+
+            string testComment = string.Format("{0}{1}{2}{3}{4}", pre, CommentParser.CommentDataPreKeyString, json, CommentParser.CommentDataPostKeyString, post);
+
+            Console.WriteLine("TEST DATA:");
+            Console.WriteLine(string.Format("JSON:" + Environment.NewLine + "{0}", json));
+            Console.WriteLine(string.Format("PRE: {0}", pre));
+            Console.WriteLine(string.Format("POST: {0}", post));
+
+            CommentParser cp = new CommentParser(testComment);
+
+            Assert.AreEqual(cd.Rating, cp.CommentData.Rating);
+            Assert.AreEqual(pre, cp.PreData);
+            Assert.AreEqual(post, cp.PostData);
+            Assert.AreEqual(json, cp.JsonString);
+
+            Console.WriteLine(Environment.NewLine + Environment.NewLine + "ACTUAL DATA:");
+            Console.WriteLine(string.Format("JSON:" + Environment.NewLine + "{0}", cp.JsonString));
+            Console.WriteLine(string.Format("PRE: {0}", cp.PreData));
+            Console.WriteLine(string.Format("POST: {0}", cp.PostData));
+        }
+
+        [TestMethod]
+        public void Comment_With_Existing_Valid_Data_No_PrePost()
+        {
+            CommentData cd = new CommentData()
+            {
+                Rating = 100
+            };
+
+            string json = JsonConvert.SerializeObject(cd);
+
+            string pre = "";
+            string post = "";
+
+            string testComment = string.Format("{0}{1}{2}{3}{4}", pre, CommentParser.CommentDataPreKeyString, json, CommentParser.CommentDataPostKeyString, post);
 
             Console.WriteLine("TEST DATA:");
             Console.WriteLine(string.Format("JSON:" + Environment.NewLine + "{0}", json));
